4f1ee9dcfae938f643cb44871c218a53783f15e3
nelo <nelokwaz@gmail.com>
2020-07-15 12:23:23 +0100

    created the type definition including the mutation to add items to cart

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,8 +1,20 @@
 import {gql} from 'apollo-boost';
+import {addItemToCart} from '../redux/cart/cart.utils';
 
+//The extend type Item is extending the Item we have
+//on our backend graphql server and it is adding a quantity prop which might or may not be available
+//the add item to cart function was added to the Mutation, which will receive an item and return an array of items which
+//might or may not be present
+//But the array must come back
 export const typeDefs = gql`
+ extend type Item{
+    quantity: Int
+ }
+ 
+ 
  extend type Mutation{
    ToggleCartHidden: Boolean!
+   AddItemToCart(item: Item!): [Item]!
  }
 `;
 
@@ -12,12 +24,18 @@ const GET_CART_HIDDEN = gql`
   cartHidden @client
 }`;
 
+const GET_CART_ITEMS = gql`
+{
+  cartItems @client
+}
+`;
+
 
 export const resolvers = {
   Mutation:{
       //cache was destructured off the _context parameter
       toggleCartHidden: (_root, _args /*{_context: {cache}}*/,{cache}, _info) => {
-          // this comes back to us as a data object just as it is defined in the index.js file and we need to destructure the cartHidden off the data Object
+          // the return of cache.readQuery comes back to us as a data object just as it is defined in the index.js file and we need to destructure the cartHidden off the data Object
           const /*{data: {cartHidden}}*/ {cartHidden} = cache.readQuery({
               query: GET_CART_HIDDEN/*,
               variables: {} If we want to pass in variables we can do that using the variables properties*/
@@ -28,6 +46,19 @@ export const resolvers = {
               data: {cartHidden: !cartHidden}
           });
           return !cartHidden;
-      }
+      },
+      //here we will pass in a variable item and destructure it off _args parameter
+      addItemToCart: (_root, {item}, {cache}) => {
+          const {cartItems} = cache.readQuery({
+              query: GET_CART_ITEMS,
+
+          });
+          const newCartItems = addItemToCart(cartItems, item);
+          cache.writeQuery({
+              query: GET_CART_ITEMS,
+              data:{cartItems: newCartItems}
+          });
+          return newCartItems;
+           }
   }
 };
--- src/index.js
+++ src/index.js
@@ -48,7 +48,8 @@ const client = new ApolloClient({
 
 client.writeData({
     data:{
-        cartHidden: true
+        cartHidden: true,
+        cartItems: []
     }
 });
 
