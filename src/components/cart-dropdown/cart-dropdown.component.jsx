32e0d8ad38c9151ac5ec1ef701bdb973533705c2
nelo <nelokwaz@gmail.com>
2020-07-15 12:31:35 +0100

    removed the redux bindings from the cart dropdown component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+/*import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';*/
 import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
+/*
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+*/
+/*
 import { toggleCartHidden } from '../../redux/cart/cart.actions.js';
+*/
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history, dispatch }) => (
+const CartDropdown = ({ cartItems, history/*, dispatch*/, toggleCartHidden }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {cartItems.length ? (
@@ -24,7 +28,8 @@ const CartDropdown = ({ cartItems, history, dispatch }) => (
     <CustomButton
       onClick={() => {
         history.push('/checkout');
-        dispatch(toggleCartHidden());
+        /*dispatch(toggleCartHidden());*/
+          toggleCartHidden();
       }}
     >
       GO TO CHECKOUT
@@ -32,8 +37,8 @@ const CartDropdown = ({ cartItems, history, dispatch }) => (
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
+/*const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems
-});
+});*/
 
-export default withRouter(connect(mapStateToProps)(CartDropdown));
+export default withRouter(/*connect(mapStateToProps)*/(CartDropdown));
